a4f12891d7efdc87c22f8a03445475cd54582f52
Hasan-TechNinja <hasantechninja@gmail.com>
2026-04-01 08:08:36 +0600

    folder structure

--- dogbuddy/settings.py
+++ dogbuddy/settings.py
@@ -105,10 +105,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dogbuddy',
-        'USER': 'dogbuddy_user',
-        'PASSWORD': 'dogbuddy_password',
-        'HOST': '127.0.0.1',
+        'NAME': 'dogbuddydatabase',
+        'USER': 'dgbuser',
+        'PASSWORD': '45rtfgdfe4erSawd12@!',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -163,13 +163,13 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.ionos.de'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = config("EMAIL_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
-DEFAULT_FROM_EMAIL = config("EMAIL_FROM")
+DEFAULT_FROM_EMAIL = config("EMAIL_FROM", default=EMAIL_HOST_USER)
 
 
 
