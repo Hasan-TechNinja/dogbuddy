892b6e23630b8ca656e8f675b4826d9cee07ca9b
Hasan-TechNinja <hasantechninja@gmail.com>
2026-04-01 14:34:03 +0600

    deploy conf

--- dogbuddy/settings.py
+++ dogbuddy/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m=(j#e%cygh^bomi#*o312l#m%#f=bqnuyrr+oz5fhhy5r@q@q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
+#ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
+
+DEBUG = False
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "developer.nett4dogs.com", "*"]
 
 
 # Application definition
@@ -92,16 +95,27 @@ CHANNEL_LAYERS = {
 }
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dogbuddy',
+#         'USER': 'dogbuddy_user',
+#         'PASSWORD': 'dogbuddy_password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dogbuddy',
+#         'USER': 'dogbuddy_user',
+#         'PASSWORD': 'dogbuddy_password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
