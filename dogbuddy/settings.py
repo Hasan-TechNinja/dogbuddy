5841b89c50e5bf7540a7e0d537e9bff2166c32e5
Hasan-TechNinja <hasantechninja@gmail.com>
2026-04-01 12:39:36 +0600

    database

--- dogbuddy/settings.py
+++ dogbuddy/settings.py
@@ -95,23 +95,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dogbuddydatabase',
-#         'USER': 'dgbuser',
-#         'PASSWORD': '45rtfgdfe4erSawd12@!',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogbuddydatabase',
+        'USER': 'dgbuser',
+        'PASSWORD': '45rtfgdfe4erSawd12@!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
